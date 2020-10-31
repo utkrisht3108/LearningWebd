03e954780895e5eadbe73ee5b6b00266c32f630d
Utkrisht Sharma <60322531+utkrisht3108@users.noreply.github.com>
2020-10-31 20:38:18 +0530

    SignUp page

---
+++ Newsletter-Signup/app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https")
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"))
+
+app.listen(process.env.PORT || 3000,function(){
+     console.log("kaam 25 chalu");
+})
+
+app.get("/",function(req,res){
+     res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+     const name = req.body.name;
+     const surname = req.body.surname;
+     const email = req.body.email;
+     var data = {
+          members: [
+               {
+                   email_address: email,
+                   status: "subscribed",
+                   merge_fields:{
+                        FNAME: name,
+                        LNAME: surname
+                   } 
+               }
+          ]
+     }
+     var jsonData = JSON.stringify(data);
+     
+     const url = 'https://us2.api.mailchimp.com/3.0/lists/8da0435ffd'
+
+     const options = {
+          method: "POST",
+          auth: "utkrisht3108:1cb177354ca87525ea32af2bdd89cc4b-us2"
+     }
+
+     const request = https.request(url,options,function(response){
+          if(response.statusCode==200){
+               res.sendFile(__dirname+"/pass.html")
+          }
+          else{
+               res.sendFile(__dirname+"/fail.html")
+          }
+          response.on("data",function(data){
+               console.log(JSON.parse(data));
+          })
+     })
+     request.write(jsonData);
+     request.end()
+     // console.log(name,surname,email);
+})
+
+app.post("/failure",function(req,res){
+     res.redirect("/");
+})
+//API key
+//1cb177354ca87525ea32af2bdd89cc4b-us2
+
+//list id
+//8da0435ffd
