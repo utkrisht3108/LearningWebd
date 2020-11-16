337fb395892fc66ed0b6c7eb66ea4a0655c0e419
Utkrisht Sharma <60322531+utkrisht3108@users.noreply.github.com>
2020-11-17 01:45:10 +0530

    Update app.js
    mongoDB

--- Blog Website/app.js
+++ Blog Website/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash")
+const mongoose = require("mongoose");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -16,29 +17,50 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+const Post = mongoose.model("Post", postSchema);
+
+
 app.get("/",function(req,res){
-  res.render("home",{
+  Post.find({},function(err,posts){
+    res.render("home",{
     startingContent: homeStartingContent,
     posts: posts
     });
+  })
   // console.log(posts);
   
 })
+app.post("/compose",function(req,res){
+  // console.log(req.body.pubText);
+  const post = new Post({
+    title: req.body.pubText,
+    content: req.body.postText
+  })
+  post.save(function(err){
+    if(!err){
+      res.redirect("/")
+    }
+  });
+})
+
+
 app.get("/posts/:check",function(req,res){
-  var reqTitle = _.lowerCase(req.params.check);
-  posts.forEach(function(post){
-    const checkTitle = _.lowerCase(post.title)
-    if(reqTitle===checkTitle){
-      res.render("post",{
-        heading: post.title,
-        content: post.postText
+  var reqTitle = (req.params.check);
+  Post.findOne({_id: reqTitle}, function(err,found){
+    res.render("post",{
+        heading: found.title,
+        content: found.content
       })
-    }
-    else{
-      console.log("Not a Match");
-    }
   })
 })
+
+
 app.get("/about",function(req,res){
     res.render("about",{abtContent: aboutContent});
 })
@@ -49,20 +71,6 @@ app.get("/contact",function(req,res){
 app.get("/compose",function(req,res){
   res.render("compose")
 })  
-app.post("/compose",function(req,res){
-  // console.log(req.body.pubText);
-  const post = {
-    title: req.body.pubText,
-    postText: req.body.postText
-  }
-  posts.push(post);
-  res.redirect("/")
-})
-
-
-
-
-
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
