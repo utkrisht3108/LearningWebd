e615e22d42f21c9e9b7dc3427393c3f69a2d0013
utkrisht3108 <utkrisht2001.sharma@gmail.com>
2021-01-19 20:05:46 +0530

    express added

--- express/server.js
+++ express/server.js
@@ -1,8 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 console.log(app.get('env'));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('chalu hogaya');
 });
