b07a4cfa5d83e19cd1dd6c2c5edbd3ec0fd9761b
utkrisht3108 <utkrisht2001.sharma@gmail.com>
2021-02-23 19:05:12 +0530

    keeper app using react start

---
+++ Keeper App/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Utkrisht</h1>
+      <p>Pawri Pawri Pawri</p>
+    </div>
+  );
+}
+
+export default Note;
