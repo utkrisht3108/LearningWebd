ae153833dc4954aa602fe833a22309b761764c02
Utkrisht <utkrisht2001.sharma@gmail.com>
2023-02-26 16:23:07 +0530

    migration base setup

---
+++ ToDoList/ToDoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    //created a constructor - write some parameters
+    //bcoz when we get the dbcontext we need to pass it on to the base class
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        //here db context is configured
+    }
+
+    //create a Db set
+    public DbSet<List> Lists { get; set; }
+}
+// we have to inherit this class file from the Db context
+// that is inside Entity Framework core
+
+// we have to add migrations in pm console to track all the db changes
---
+++ ToDoList/ToDoList/Models/List.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class List
+    {
+        //make Id column an identity column which is the primary key
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
